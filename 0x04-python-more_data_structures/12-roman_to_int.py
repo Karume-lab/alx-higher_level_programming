099b5e012f2eebeb137dae4ed3f6ac63a0e5d75b
Karume-lab <danielkarume@outlook.com>
2023-03-19 18:37:18 +0300

    Roman to int

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,27 +2,27 @@
 def roman_to_int(roman_string):
     if not roman_string:
         return 0
-    roman_string_upper = roman_string.upper()
+    if not isinstance(roman_string, str):
+        return 0
+
     roman_dict = {
-        "I": 1,
-        "V": 5,
-        "X": 10,
-        "L": 50,
-        "C": 100,
-        "D": 500,
-        "M": 1000,
-    }
-    num = roman_dict.get(roman_string_upper[0])
-    roman_string_upper = roman_string_upper[1:]
-    for i, roman in enumerate(roman_string_upper):
-        rom = roman_dict.get(roman_string_upper[i])
-        if roman not in roman_dict:
-            return 0
-        if num >= rom:
-            num += rom
-            if len(roman_string) > 3:
-                if i == 1 and rom < roman_dict.get(roman_string_upper[i+1]):
-                    num -= 2
-        else:
-            num = roman_dict.get(roman_string_upper[i]) - num
-    return num
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+            }
+    roman_keys = list(roman_dict.keys())
+    roman_string = roman_string[::-1]
+    value = roman_dict[roman_string[0]]
+    for i in range(1, len(roman_string)):
+        r = roman_string[i]
+        s = roman_string[i - 1]
+        if r in roman_keys and s in roman_keys:
+            if roman_dict[r] >= roman_dict[s]:
+                value += roman_dict[r]
+            else:
+                value -= roman_dict[r]
+    return value
