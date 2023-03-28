0d54a027e490c2385fd1082cead885d4a1ebf90b
Karume-lab <danielkarume@outlook.com>
2023-03-28 13:04:45 +0300

    Size validation

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    def __init__(self, size=0):
+        if not type(size) is int:
+            raise TypeError("size must be an integer")
+        elif 0 > size:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
