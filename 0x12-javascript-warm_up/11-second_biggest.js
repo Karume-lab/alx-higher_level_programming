9c86e652f5f28a1695649f9fe7394308be471495
Karume-lab <danielkarume@outlook.com>
2023-06-27 16:26:55 +0300

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 
 let big2 = process.argv[2];
-if (big2) {
+if (big2 && (parseInt(big2))) {
   for (let index = 2; index < process.argv.length; index++) {
     if ((big2 < process.argv[index]) && (process.argv[index] !== process.argv[process.argv.length - 1])) {
       big2 = process.argv[index];
