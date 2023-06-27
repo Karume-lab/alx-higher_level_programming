4f51a22ca9e32688ba21ec78f112aca045acea9b
Karume-lab <danielkarume@outlook.com>
2023-06-27 16:25:36 +0300

    Second biggest!

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 
 let big2 = process.argv[2];
-for (let index = 2; index < process.argv.length; index++) {
-  if ((big2 < process.argv[index]) && (process.argv[index] !== process.argv[process.argv.length - 1])) {
-    big2 = process.argv[index];
+if (big2) {
+  for (let index = 2; index < process.argv.length; index++) {
+    if ((big2 < process.argv[index]) && (process.argv[index] !== process.argv[process.argv.length - 1])) {
+      big2 = process.argv[index];
+    }
   }
+} else {
+  big2 = 0;
 }
 console.log(big2);
